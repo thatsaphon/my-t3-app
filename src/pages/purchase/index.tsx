e15a90d90f53faa4ac2c4d7df4a5005f993f586c
Thatsaphon Sukanantatham <thatsaphon@profilegroup-hrs.com>
2023-03-07 17:09:54 +0700

    clone ar to ap

--- src/pages/purchase/index.tsx
+++ src/pages/purchase/index.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { Calendar } from "primereact/calendar";
+import TransactionList from "~/components/TransactionList";
+import SalesDialog from "~/components/SalesDialog";
 
 function PurchasePage() {
-  return <div>PurchasePage</div>;
+  const [dates, setDates] = useState(null);
+
+  return (
+    <>
+      <div className="flex h-full flex-col items-start">
+        <div className="flex w-full gap-2 bg-gray-200 p-2">
+          <SalesDialog />
+          <div className="border-2 border-gray-500 p-2 underline">
+            + ซื้อเงินเชื่อ
+          </div>
+        </div>
+        <div className="flex flex-1 justify-start">
+          <div className="border-r-black-200 flex h-full w-[300px] flex-col items-center border-r-2 p-2">
+            <Calendar
+              selectionMode="range"
+              value={dates}
+              onChange={(e) => setDates(e.value)}
+              view="month"
+              className="mt-2 w-full"
+              dateFormat="mm/yy"
+              inline
+            />
+            <Calendar
+              selectionMode="range"
+              value={dates}
+              onChange={(e) => setDates(e.value)}
+              view="month"
+              className="mt-2"
+              dateFormat="mm/yy"
+            />
+            <div>test</div>
+          </div>
+          <div className="w-full">
+            <TransactionList />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default PurchasePage;
